663e91e448ce0b8aae655ecc035df18683820318
Jena SanCartier and Katy Henning <katy@>
2016-08-11 15:06:25 -0700

    add front end JavaScript for isoceles results

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,27 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     var side1 = parseInt($("input#uno1").val());
-      alert(side1)
+      // alert(side1)
+    var side2 = parseInt($("input#dos2").val());
+      // alert(side2)
+    var side3 = parseInt($("input#tres3").val());
+      // alert(side3)
+
+    if (side1 === side2 && side2 === side3){
+      alert("Your triangle is equilateral")
+    }
+    else if (side1 === side2 && side2 != side3){
+      alert("Your triangle is an isoceles")
+    }
+    else if (side3 === side2 && side2 != side1){
+      alert("Your triangle is an isoceles")
+    }
+    else if (side1 === side3 && side3 != side2){
+      alert("Your triangle is an isoceles")
+    }
+
+
 
    event.preventDefault();
   });
-
-
-    //  var side1 = $ parseInt("input#uno1").val();
-    //  alert(side1)
-
-  //  event.preventDefault();
   });
