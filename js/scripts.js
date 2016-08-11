6de9bc96ace7e4c8aa01f65ab2976d5697fa0e08
Jena SanCartier and Katy Henning <jena@>
2016-08-11 14:32:40 -0700

    add initial files and folders, created forms, begin writing front end JavaScript for first form field

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+//back end
+
+//front end
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    var side1 = parseInt($("input#uno1").val());
+      alert(side1)
+
+   event.preventDefault();
+  });
+
+
+    //  var side1 = $ parseInt("input#uno1").val();
+    //  alert(side1)
+
+  //  event.preventDefault();
+  });
